a4f356754f8e526c5518c703200ae199519bd25f
Dania Dobbs <dania.dobbs@mymona.uwi.edu>
2017-11-18 03:56:45 +0000

    country lookup working

--- world.php
+++ world.php
@@ -8,19 +8,24 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 $stmt = $conn->query("SELECT * FROM countries");
-$q= query("SELECT * FROM countries WHERE name LIKE %$country%");
+
+
+if (!empty($_GET)){ 
+  if (array_key_exists("all", $_GET)){ 
+    $stmt = $conn -> query ("SELECT * FROM countries"); 
+  } 
+  else if(array_key_exists("country", $_GET)){ 
+     $country = $_GET['country']; 
+     $stmt = $conn->query("SELECT * FROM countries WHERE name = '{$country}'"); 
+  }
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
-
-$request = $_GET[$q];
-echo $request;
-
-
-
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>'; 
+    
 }
-echo '</ul>'; 
